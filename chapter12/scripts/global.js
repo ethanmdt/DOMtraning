e99385f82d9f965acf3c570b8434fb5ddf9477ec
Ethan <ethanmdt@gmail.com>
2014-07-10 22:35:51 +0800

    chapter12 new

--- chapter12/scripts/global.js
+++ chapter12/scripts/global.js
@@ -91,5 +91,46 @@ function moveElement(elementID,final_x,final_y,interval) {
   elem.movement = setTimeout(repeat,interval);
 }
 
-
+function preSlideshow() {
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("intro")) return false;
+	var  intro = document.getElementById("intro");
+	var slideshow = document.createElement("div");
+	slideshow.setAttribute("id", "slideshow");
+	var frame = document.createElement("img");
+	frame.setAttribute("src","images/frame.gif");
+	frame.setAttribute("alt","");
+	frame.setAttribute("id","frame");
+	slideshow.appendChild(frame);
+	var preview = document.createElement("img");
+	preview.setAttribute("src", "images/slideshow.gif");
+	preview.setAttribute("alt", "a glimpse of what awaits you");
+	preview.setAttribute("id","preview");
+	slideshow.appendChild(preview);
+	insertAfter(slideshow,intro);
+	var links = document.getElementsByTagName("a");
+	var destination;
+	for (var i = 0; i < links.length; i++) {
+		links[i].onmouseover = function(){
+			destination = this.getAttribute("href");
+			if (destination.indexOf("index.html") != -1) {
+				moveElement("preview",0,0,5);
+			} 
+			if (destination.indexOf("about.html") != -1) {
+				moveElement("preview",-150,0,5);
+			} 
+			if (destination.indexOf("photos.html") != -1) {
+				moveElement("preview",-300,0,5);
+			}
+			if (destination.indexOf("live.html") != -1) {
+				moveElement("preview",-450,0,5);
+			}
+			if (destination.indexOf("contact.html") != -1) {
+				moveElement("preview",-600,0,5);
+			}
+		}
+	}
+}
 addLoadEvent(highlightPage);
+addLoadEvent(preSlideshow);
